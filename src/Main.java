926e1bc538e93b7ecdda5e58e1dc3b9e18e2c192
omarebid1 <omar3bid18@gmail.com>
2023-07-17 17:32:10 +0200

    palindrome file

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.lang.String;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("inserisci una parola");
+        String word = scan.nextLine();
+
+       // StringBuilder revWord = new StringBuilder(word);
+
+        String revWord;
+
+        /*if (word.equals(revWord)){
+            System.out.println("è una parola palindrome");
+
+        }
+
+        else {
+            System.out.println("non è una parola palindrome");
+        }
+
+         */
+
+
+
+    }
+}
